5aa02b673d6f43d70ac69eb159b1ae96ae6967fc
matte21 <matteoolivi7@gmail.com>
2025-09-22 17:03:22 -0400

    Fix bug

--- pkg/kubelet/cm/farmemtopologymanager/manager.go
+++ pkg/kubelet/cm/farmemtopologymanager/manager.go
@@ -416,20 +416,12 @@ func (m *Manager) allocateCPUs(nNUMAs []int, numToAlloc int, cacheDist string, w
 			updateCoresAndLLCsMaps(n, cs.List())
 		}
 
-		klog.InfoS("Allocating CPUs from numa %d: %v", nID, cs.List())
-
 		cpuGivers[nID] = cs.Size()
 
 		allocatedCPUs = allocatedCPUs.Union(cs)
 		n.ReservedCPUs = n.ReservedCPUs.Union(cs)
 		n.FreeCPUs = n.FreeCPUs.Difference(cs)
 		m.defaultCPUSetChanged = true
-
-		klog.InfoS("numa %d free: %v", nID, n.FreeCPUs.List())
-		klog.InfoS("numa %d idle cores: %v", nID, n.IdleCoresToCPUs)
-		klog.InfoS("numa %d busy cores: %v", nID, n.BusyCoresToFreeCPUs)
-		klog.InfoS("numa %d idle LLCs: %v", nID, n.IdleLLCsToCPUs)
-		klog.InfoS("numa %d busy LLCs: %v", nID, n.BusyLLCsToFreeCPUs)
 	}
 
 	heap.Init(m.topo.nNUMAsByFreeCPUs)
@@ -587,7 +579,7 @@ func (m *Manager) getCPUsL1Spread(n *nNUMANode, remainingToAlloc int) []int {
 		updateCoresAndLLCsMaps(n, allocated)
 	}()
 
-	for cpu := range n.FreeCPUs.List() {
+	for _, cpu := range n.FreeCPUs.List() {
 		allocated = append(allocated, cpu)
 		remainingToAlloc--
 		if remainingToAlloc == 0 {
